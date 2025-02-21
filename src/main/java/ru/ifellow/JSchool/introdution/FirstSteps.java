05081e89c6371003486d5629213c5571449d1292
Kurbanmagomed95 <163135644+Kurbanmagomed95@users.noreply.github.com>
2025-02-21 12:45:31 +0300

    ready

--- src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
+++ src/main/java/ru/ifellow/JSchool/introdution/FirstSteps.java
@@ -1,89 +1,226 @@
 package ru.ifellow.JSchool.introdution;
 
 public class FirstSteps {
-
-    public int sum (int x, int y){
-
-    }
-
-    public int mul (int x, int y){
+    public static void main(String[] args) {
 
     }
+    public static int sum (int x, int y){
 
-    public int div (int x, int y){
-
+        return x + y;
     }
 
-    public int mod (int x, int y){
+   public static int mul (int x, int y){
 
+        return x * y;
     }
 
-    public boolean isEqual (int x, int y){
+    public static int div (int x, int y){
 
+        return x / y;
     }
 
-    public boolean isGreater (int x, int y){
+    public static int mod (int x, int y){
 
+        return x % y;
     }
 
-    public boolean isInsideRect(int xLeft, int yTop, int xRight, int yBottom, int x, int y){
+    public static boolean isEqual (int x, int y){
+        /*
+        Это один способ выполнения
 
+        if (x == y){
+            return true;
+        }else{
+            return false;
+        }
+         */
+        return x == y;
     }
 
-    public int sum(int[] array){
+    public static boolean isGreater (int x, int y){
+        /*
+        Это один способ выполнения
 
+        if(x > y){
+            return true;
+        }else{
+            return false;
+        }
+        */
+        return x > y;
     }
 
-    public int mul(int[] array){
+    public static boolean isInsideRect(int xLeft, int yTop, int xRight, int yBottom, int x, int y){
 
+        return x >= xLeft && x <= xRight && y >= yTop && y <= yBottom;
     }
 
-    public int min(int[] array){
+    public static int sum(int[] array){
 
+        int sumNumber = 0;
+
+        if (array.length == 0) {
+            return 0;
+        }
+
+        for(int num : array){
+            sumNumber += num;
+        }
+        return sumNumber;
     }
 
-    public int max(int[] array){
+     public static int mul(int[] array){
 
+        if (array.length == 0) {
+                return 0;
+        }
+        int mulNumber = 1;
+        for (int num : array) {
+            mulNumber *= num;
+        }
+        return mulNumber;
     }
 
-    public double average(int[] array){
+    public static int min(int[] array){
 
+        int minValue = array[0];
+
+        if (array.length == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        for (int num : array) {
+            if (num < minValue) {
+                minValue = num;
+            }
+        }
+        return minValue;
     }
 
-    public boolean isSortedDescendant(int[] array){
+    public static int max(int[] array){
 
+        int maxValue = array[0];
+
+        if (array.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        for (int num : array) {
+            if (num > maxValue) {
+                maxValue = num;
+            }
+        }
+        return maxValue;
     }
 
-    public void cube(int[]array){
+    public static double average(int[] array){
 
+        int sumNumber = 0;
+
+        if (array.length == 0) {
+            return 0;
+        }
+        for(int num : array){
+            sumNumber += num;
+        }
+        return (double) sumNumber / array.length;
     }
 
-    public boolean find(int[]array, int value){
+    public static boolean isSortedDescendant(int[] array){
 
+        for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] <= array[i + 1]) {
+                    return false;
+                }
+        }
+        return true;
     }
 
-    public void reverse(int[]array){
-
+    public static void cube(int[]array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * array[i] * array[i];
+        }
+    }
+    public static boolean find(int[]array, int value){
+               for (int num : array) {
+                    if (num == value) {
+                        return true;
+                    }
+               }
+               return false;
     }
 
-    public boolean isPalindrome(int[]array){
+    public static void reverse(int[]array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
 
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 
-    public int sum(int[][] matrix){
-
+   public static boolean isPalindrome(int[]array){
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            if (array[left] != array[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
-    public int max(int[][] matrix){
-
+   public static int sum(int[][] matrix){
+        int sumNumber = 0;
+        for (int[] line : matrix) {
+            for (int num : line) {
+                sumNumber += num;
+            }
+        }
+        return sumNumber;
     }
 
-    public int diagonalMax(int[][] matrix){
-
+    public static int max(int[][] matrix){
+        int maxValue = Integer.MIN_VALUE;
+        if (matrix.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        for (int[] line : matrix) {
+            for (int num : line) {
+                if (num > maxValue) {
+                maxValue = num;
+                }
+            }
+        }
+        return maxValue;
     }
 
-    public boolean isSortedDescendant(int[][] matrix){
+     public static int diagonalMax(int[][] matrix){
+         int max = Integer.MIN_VALUE;
+         if (matrix.length == 0) {
+             return Integer.MIN_VALUE;
+         }
+         for (int i = 0; i < matrix.length; i++) {
+             max = Math.max(max, matrix[i][i]);
+         }
 
+         return max;
+    }
+
+    public static boolean isSortedDescendant(int[][] matrix){
+
+        for (int[] line : matrix) {
+            if (!isSortedDescendant(line)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
